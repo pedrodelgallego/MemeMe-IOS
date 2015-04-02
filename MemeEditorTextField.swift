a0822f92351324495fd25952b4023b39e5cf9e9f
Pedro Del Gallego <pedro.delgallego@gmail.com>
2015-04-02 18:16:42 +0200

    meme editor controllers

---
+++ MemeEditorTextField.swift
@@ -0,0 +1,36 @@
+//
+//  MemeEditorTextField.swift
+//  MemeMe-IOS
+//
+//  Created by Pedro Del Gallego on 02/04/15.
+//  Copyright (c) 2015 Pedro Del Gallego. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeEditorTextField: NSObject, UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.whiteColor(),
+        NSForegroundColorAttributeName: UIColor.redColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 38)!,
+        NSStrokeWidthAttributeName : 3.0
+    ]
+    
+    func configTextField(textField: UITextField, text: String){
+        textField.delegate = self
+        textField.backgroundColor = UIColor.clearColor()
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .Center
+        textField.text = text
+    }
+}
--- MemeMe-IOS/ViewController.swift
+++ MemeMe-IOS/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var MemeEditorView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    let memeEditorTextField = MemeEditorTextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        memeEditorTextField.configTextField(topTextField, text: "TOP")
+        memeEditorTextField.configTextField(bottomTextField, text: "BOTTOM")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func shareMeme(sender: UIBarButtonItem) { }
+    
+    @IBAction func cancel(sender: UIBarButtonItem) { }
+    
+    @IBAction func pickAPicture(sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .PhotoLibrary
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
 
+    @IBAction func takeAPicture(sender: UIBarButtonItem) { }
+    
+    // PRAGMA: UIImagePickerControllerDelegate,UINavigationControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = image
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
